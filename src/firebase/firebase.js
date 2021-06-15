10872e487e667367eb8b0c0a9173be01812df199
春日崇希 <kasugatakashinozomi@kasugatakashinozominoMacBook-Pro.local>
2021-06-15 14:01:22 +0900

    firebaseの環境構築

---
+++ src/firebase/firebase.js
@@ -0,0 +1,13 @@
+import firebase from 'firebase'
+
+// Your web app's Firebase configuration
+var firebaseConfig = {
+  apiKey: "AIzaSyBUEXTUE4flvy9hwVIlmyFGl4ErZ-DrbKI",
+  authDomain: "ec-react-typescript.firebaseapp.com",
+  projectId: "ec-react-typescript",
+  storageBucket: "ec-react-typescript.appspot.com",
+  messagingSenderId: "585697143162",
+  appId: "1:585697143162:web:98ba3532ff48648ad54ec3"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
