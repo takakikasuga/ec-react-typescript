49b29a02860ae9d127407ad0e32cb80b9b9560a3
春日崇希 <kasugatakashinozomi@kasugatakashinozominoMacBook-Pro.local>
2021-06-23 16:43:19 +0900

    完成版ーver

--- src/test/CartTableRowContents.test.js
+++ src/test/CartTableRowContents.test.js
@@ -23,31 +23,32 @@ describe("CartリストのTableRowContentsコンポーネント", () => {
     )
   })
 
-
   // 各処理が通った後にアンマウントさせる
   afterEach(() => cleanup())
 
-  describe.only("cartリストのrowコンテンツの確認", () => {
+  describe("cartリストのrowコンテンツの確認", () => {
     test("rowコンテンツの金額が3000円であることの表示確認（パターン１）", () => {
       // screen.debug()
       screen.debug(screen.getByTestId("price"))
-      expect(screen.getByTestId("price")).toBeInTheDocument("3,000円")
+      // 完全一致パターン
+      expect(screen.getByTestId("price")).toHaveTextContent(/^3,000円$/)
     })
     test("rowコンテンツの金額が3000円であることの表示確認（パターン２）", () => {
       // screen.debug()
       screen.debug(screen.getByTestId("price"))
+      // textContentはDOM要素のtextContentにアクセス
       expect(screen.getByTestId("price").textContent).toBe("3,000円")
     })
     test("rowコンテンツの金額が3000円であることの表示確認（パターン３）", () => {
       // screen.debug()
       screen.debug(screen.getByTestId("price"))
-      expect(screen.getByTestId("price")).toHaveTextContent("3,000円")
+      // .toBeInTheDocumentはDOM要素があるか否かの確認
+      expect(screen.getByTestId("price")).toBeInTheDocument()
     })
     test("rowコンテンツの個数が3個であることの表示確認", () => {
       screen.debug()
       screen.debug(screen.getByTestId("count"))
-
-      expect(screen.getByTestId("count")).toBeInTheDocument("3個")
+      expect(screen.getByTestId("count").innerHTML).toBe("3個")
     })
     test("rowコンテンツに存在するtdタグの数をテスト", () => {
       expect(screen.getAllByRole("cell")).toHaveLength(5)
